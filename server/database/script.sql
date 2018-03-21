1eadf5bba3b0bb39764296c7f9c0b2e93b0756a7
William Kwasidev <kwesidev@gmail.com>
2018-03-21 10:55:10 +0200

    database sample

---
+++ server/database/script.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE patients(
+    id SERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    id_number VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE admins(
+
+    id SERIAL NOT NULL PRIMARY KEY,
+    username VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL
+);
+
+INSERT INTO patients(first_name,last_name,id_number) VALUES('Anabelle','Annabelle','9411210001123');
+INSERT INTO admins(username,password) VALUES('kwasidev','$2y$10$kLPbMCGfqo4JJjQLnbBnOOwN2wUYPHXNzj4QQJ./E5T7riitoD6vm');
+
